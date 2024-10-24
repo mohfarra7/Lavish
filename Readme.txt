568d51113376cada3b53460905934e1e41d51be9
mohfarra7 <your-email@example.com>
2024-10-24 10:06:52 +0200

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,20 @@
+The cloudinary and mongo will work in a very short time, so please make sure to update cloudinary and mongo APIs.
+You will find the ".env" file in the backend.
+
+At VsCode seperate the terminals of three folders:
+Firstly run the backend server, but install the node package first
+
+npm install
+
+npm run server
+
+
+For the adminpanel and then frontend, both are in vite:
+
+npm install
+
+npm run dev
+ 
+# Admin Panel Credentials
+ADMIN_EMAIL = "admin@lavish.com"
+ADMIN_PASSWORD = "admin123"
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+              <p>Lavish yearns for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of luxurious fashion.</p>
+              <p>We are offering a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty we offer an extensive collection sourced from trusted brands and suppliers.</p>
+              <b className='text-gray-800'>Our Mission</b>
+              <p>Our mission at Lavish is to empower customers with choice, convenience, and confidence.</p>
+          </div>
+      </div>
+
+      <div className=' text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our quality standards.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className=' text-gray-600'>With our friendly order process, shopping has never been easier.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className=' text-gray-600'>Our team of dedicated professionals assists you the way ensuring your needs and satisfaction are our top priority.</p>
+          </div>
+      </div>
+
+      <NewsletterBox/>
+      
+    </div>
+  )
+}
+
+export default About
